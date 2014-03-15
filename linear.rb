f39eced7b8d03d9f7220aabaaad211189d69587a
Vladimir <vova@getbase.com>
2014-03-15 13:00:51 +0100

    convert spaces (4 to 2)

--- linear.rb
+++ linear.rb
@@ -1,86 +1,86 @@
 module Regression
-    class Linear
-        # Expected value
-        # http://en.wikipedia.org/wiki/Expected_value
-        def ev(items)
-            raise "Items must be an array" unless items.is_a?(Array)
+  class Linear
+    # Expected value
+    # http://en.wikipedia.org/wiki/Expected_value
+    def ev(items)
+      raise "Items must be an array" unless items.is_a?(Array)
 
-            items.inject(0) {|sum, x| sum + x}.to_f / items.length
-        end
-
-        # Covariance
-        # http://en.wikipedia.org/wiki/Covariance
-        def cov(xs, ys)
-            raise "Length xs and ys must be equal" unless xs.length == ys.length
-
-            xys = (0..(xs.length-1)).map{|i| xs[i] * ys[i]}
-            ev(xys) - ev(xs)*ev(ys)
-        end
-
-        # Another way to implement covariance
-        def cov2(xs, ys)
-            raise "Length xs and ys must be equal" unless xs.length == ys.length
-
-            len = xs.length
-            sum = 0 
-            ev_x = ev(xs)
-            ev_y = ev(ys)
-
-            0.upto(len - 1) do |i|
-               sum += (xs[i].to_f - ev_x) * (ys[i].to_f - ev_y)
-            end
-            sum / len
-        end
-
-
-        # Variance
-        # http://en.wikipedia.org/wiki/Variance
-        def var(items)
-            raise "Items must be an array" unless items.is_a?(Array)
-
-            ev(items.map{|i| i**2}) - ev(items)**2
-        end
-
-        # Standard Deviation
-        # http://en.wikipedia.org/wiki/Standard_deviation
-        def stdev(items)
-            raise "Items must be an array" unless items.is_a?(Array)
-
-            Math.sqrt(var(items))
-        end
-
-        # Pearson product-moment correlation coefficient
-        # http://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
-        def pcc(xs, ys)
-            raise "Length xs and ys must be equal" unless xs.length == ys.length
-
-            cov(xs, ys) / (stdev(xs).to_f * stdev(ys).to_f)
-        end
-
-        # Spearman's rank correlation coefficient
-        # http://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient
-        def scc(xs, ys)
-            raise "Length xs and ys must be equal" unless xs.length == ys.length
-            
-            len = xs.length
-            sum = 0.0
-            (0..(len-1)).each do |i|
-                sum += (xs[i] - ys[i]) ** 2
-            end
-
-            1 - (6 * sum)/(len * (len - 1))
-        end
-
-        # y = kx + b
-        def k(xs, ys)
-            raise "Length xs and ys must be equal" unless xs.length == ys.length
-
-            cov(xs, ys) / var(xs)   
-        end
-
-        # y = kx + b
-        def b(xs, ys)
-            ev(ys) - k(xs, ys) * ev(xs)
-        end
+      items.inject(0) {|sum, x| sum + x}.to_f / items.length
     end
+
+    # Covariance
+    # http://en.wikipedia.org/wiki/Covariance
+    def cov(xs, ys)
+      raise "Length xs and ys must be equal" unless xs.length == ys.length
+
+      xys = (0..(xs.length-1)).map{|i| xs[i] * ys[i]}
+      ev(xys) - ev(xs)*ev(ys)
+    end
+
+    # Another way to implement covariance
+    def cov2(xs, ys)
+      raise "Length xs and ys must be equal" unless xs.length == ys.length
+
+      len = xs.length
+      sum = 0 
+      ev_x = ev(xs)
+      ev_y = ev(ys)
+
+      0.upto(len - 1) do |i|
+         sum += (xs[i].to_f - ev_x) * (ys[i].to_f - ev_y)
+      end
+      sum / len
+    end
+
+
+    # Variance
+    # http://en.wikipedia.org/wiki/Variance
+    def var(items)
+      raise "Items must be an array" unless items.is_a?(Array)
+
+      ev(items.map{|i| i**2}) - ev(items)**2
+    end
+
+    # Standard Deviation
+    # http://en.wikipedia.org/wiki/Standard_deviation
+    def stdev(items)
+      raise "Items must be an array" unless items.is_a?(Array)
+
+      Math.sqrt(var(items))
+    end
+
+    # Pearson product-moment correlation coefficient
+    # http://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
+    def pcc(xs, ys)
+      raise "Length xs and ys must be equal" unless xs.length == ys.length
+
+      cov(xs, ys) / (stdev(xs).to_f * stdev(ys).to_f)
+    end
+
+    # Spearman's rank correlation coefficient
+    # http://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient
+    def scc(xs, ys)
+      raise "Length xs and ys must be equal" unless xs.length == ys.length
+      
+      len = xs.length
+      sum = 0.0
+      (0..(len-1)).each do |i|
+        sum += (xs[i] - ys[i]) ** 2
+      end
+
+      1 - (6 * sum)/(len * (len - 1))
+    end
+
+    # y = kx + b
+    def k(xs, ys)
+      raise "Length xs and ys must be equal" unless xs.length == ys.length
+
+      cov(xs, ys) / var(xs)   
+    end
+
+    # y = kx + b
+    def b(xs, ys)
+      ev(ys) - k(xs, ys) * ev(xs)
+    end
+  end
 end
